7e99df7e4ff7f2b1b67eaac281ba30ff301573f0
Charles <i_charles@foxmail.com>
2015-01-13 19:49:24 +0800

    Initial Commit

---
+++ SlideViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideViewController
+//
+//  Created by Charles on 15/1/13.
+//  Copyright (c) 2015年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideViewController
+//
+//  Created by Charles on 15/1/13.
+//  Copyright (c) 2015年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
